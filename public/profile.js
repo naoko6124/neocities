1905d1c8fa2fcb3d4464ff948b702e1e67adf8c0
Naoko <97308762+naoko6124@users.noreply.github.com>
2025-07-08 20:26:05 -0300

    count

---
+++ public/profile.js
@@ -0,0 +1,21 @@
+fetch('./profile.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('sidebar').innerHTML += data;
+    });
+
+fetch('./game.json').then(response => response.json()).then(data => {
+    document.getElementById('game-count').innerHTML += data.completed.length;
+});
+
+fetch('./vn.json').then(response => response.json()).then(data => {
+    document.getElementById('vn-count').innerHTML += data.completed.length;
+});
+
+fetch('./anime.json').then(response => response.json()).then(data => {
+    document.getElementById('anime-count').innerHTML += data.completed.length;
+});
+
+fetch('./manga.json').then(response => response.json()).then(data => {
+    document.getElementById('manga-count').innerHTML += data.completed.length;
+});
